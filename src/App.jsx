5437a01dbebdf49c817fc3dab1aefc256f9b782f
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-07-09 23:42:29 -0300

    Fix: import react implicitly

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import React from "react";
+import { TopBar } from "./topbar";
+
 export default function App() {
   return (
     <main>
+      <TopBar />
       <h1>App packed with webpack</h1>
       <p>This web app was made from scratch with Webpack</p>
     </main>
