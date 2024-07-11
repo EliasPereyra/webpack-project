fc7f1073c3f1b804db40c117fefff0ad7cf7342c
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-07-11 09:29:57 -0300

    Get users from a api as example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import React from "react";
-import { TopBar } from "./topbar";
+import React, { useEffect, useState } from "react";
+
+import { TopBar } from "./topbar.jsx";
+
+import "./App.css";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const getUsers = () => {
+    fetch(process.env.JSONPLACEHOLDER_API)
+      .then((res) => {
+        if (!res.ok) return;
+        return res.json();
+      })
+      .then((data) => setUsers(data));
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <main>
+    <main className="main-wrapper">
       <TopBar />
-      <h1>App packed with webpack</h1>
-      <p>This web app was made from scratch with Webpack</p>
+      <div className="mw__headline-content">
+        <h1>App packed with webpack</h1>
+        <p>This web app was made from scratch with Webpack 5</p>
+      </div>
+      <div className="mw__users-container">
+        <h3>Users from the JSONPlaceholder API</h3>
+        <p>Test data, fetching data from a environment variable</p>
+        <ul className="mw__users">
+          {users.map((user) => (
+            <li key={user.id}>
+              <article>
+                <h4>{user.name}</h4>
+                <p>{user.email}</p>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
